086cc147db325f4a136b035f0b7e52c3137e096b
TrubchikVany <123826041+TrubchikVany@users.noreply.github.com>
2026-03-09 02:31:48 +0300

    first commit

--- .history/mini-app/src/pages/HealthPage_20260221225540.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC } from 'react';
-
-export const HealthPage: FC = () => (
-  <div style={{ padding: 16 }}>
-    <h1 style={{ margin: 0, fontSize: 22 }}>Health</h1>
-    <p style={{ marginTop: 8, color: '#666' }}>Экран-заглушка</p>
-  </div>
-);
