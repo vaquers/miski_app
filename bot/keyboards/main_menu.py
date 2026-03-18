e938de42b419d59d1a6bb365c76a585153641e0f
TrubchikVany <123826041+TrubchikVany@users.noreply.github.com>
2026-03-19 02:19:22 +0300

    2

--- bot/keyboards/main_menu.py
+++ bot/keyboards/main_menu.py
@@ -3,7 +3,7 @@ from aiogram.fsm.context import FSMContext
 from datetime import datetime
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
-from config import WEBAPP_URL
+from config import get_webapp_url
 
 
 async def regime_dnevnik(message: types.Message, state: FSMContext = None):
@@ -29,7 +29,7 @@ async def main_menu():
         [
             types.KeyboardButton(
                 text="📖 дневник",
-                web_app=WebAppInfo(url=WEBAPP_URL),
+                web_app=WebAppInfo(url=get_webapp_url()),
             )
         ],
         [types.KeyboardButton(text="🧠 TudoAI")]
