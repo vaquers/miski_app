76ef78290023acc3db2cc1423430fccc679b5d91
TrubchikVany <123826041+TrubchikVany@users.noreply.github.com>
2026-03-19 06:27:52 +0300

    change2

--- mini-app/src/vite-env.d.ts
+++ mini-app/src/vite-env.d.ts
@@ -1,4 +1,9 @@
 /// <reference types="vite/client" />
 
+declare module '*.JPG' {
+  const src: string;
+  export default src;
+}
+
 declare const __BUILD_COMMIT__: string;
 declare const __BUILD_TIME__: string;
